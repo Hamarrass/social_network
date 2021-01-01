72c0fb80abec90136e93fd6a4be946c05180d793
Hamarrass <hassanhamarrass@gmail.com>
2021-01-01 12:48:50 +0100

    add layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+  @section('countent')
+    <h1>About page</h1>
+    <p>About me</p>
+    @endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+  @section('countent')
+
+  <h1>Home page</h1>
+  <p>Learn Laravel</p>
+
+  @endsection
+
